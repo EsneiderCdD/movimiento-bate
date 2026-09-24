a36cc8eb70b18712302724e209c9b3e017af63be
EsneiderCdD <ecd.97@hotmail.com>
2026-09-23 19:03:30 -0500

    feat(hero)

--- src/components/specific/hero/Hero.tsx
+++ src/components/specific/hero/Hero.tsx
@@ -10,22 +10,24 @@ interface HeroProps {
 export default function Hero({ onSelect, selected }: HeroProps) {
   return (
     <section className={styles.hero}>
-      <Image
-        src="/logo.png"
-        alt="Movimiento Bate"
-        width={1280}
-        height={1280}
-        priority
-        className={styles.logo}
-      />
-      <h1 className={styles.title}>Movimiento Bate</h1>
-      <p className={styles.text}>
-        El Movimiento del Bate nace del deseo de miles de ciudadanos de
-        recuperar el orden, la autoridad y el respeto por Medellín y Antioquia.
-        Surge como un símbolo de resistencia ciudadana, no de violencia, sino de
-        defensa frente al vandalismo, la corrupción y el miedo que han intentado
-        imponerse en las calles.
-      </p>
+      <div className={styles.intro}>
+        <Image
+          src="/logo.png"
+          alt="Movimiento Bate"
+          width={1280}
+          height={1280}
+          priority
+          className={styles.logoBg}
+        />
+        <h1 className={styles.title}>Movimiento Bate</h1>
+        <p className={styles.text}>
+          El Movimiento del Bate nace del deseo de miles de ciudadanos de
+          recuperar el orden, la autoridad y el respeto por Medellín y Antioquia.
+          Surge como un símbolo de resistencia ciudadana, no de violencia, sino
+          de defensa frente al vandalismo, la corrupción y el miedo que han
+          intentado imponerse en las calles.
+        </p>
+      </div>
       <div className={styles.imageWrapper}>
         <Image
           src="/FONDO2.jpg"
